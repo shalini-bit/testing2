1adcdf048c2101f9d2753003bc5993ef0eb85189
shalini-bit <shalinis0205@gmail.com>
2023-10-02 10:01:36 +0530

    progress bar added

---
+++ testing.js
@@ -0,0 +1,21 @@
+bookRating = 4.2;
+bookGenre = "Crime";
+
+if (bookRating > 4.0 || bookGenre == "Crime")
+{
+    console.log ("Buy book");
+}else{
+    console.log("Don't buy book");
+}
+
+
+let a = prompt("enter the first number:");
+let b  =prompt("enter the secon number:");
+let c = prompt("enter the third number:");
+
+if(a ** 2 + b ** 2 == c**2){
+    console.log("It is a pythogorean triple.")
+}else{
+    console.log ("It is a not a pythogorean triple.")
+}
+
